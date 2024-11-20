551c7dd5e94f7d04a349e16207d702719c98af74
tekteacher <contact@tekteacher.com>
2024-11-19 21:24:15 -0500

    2nd commit

--- hello-powershell/hellops.ps1
+++ hello-powershell/hellops.ps1
@@ -1,1 +1,3 @@
 echo "Hello World!"
+$res_op = Get-Service | Where DisplayName -like "*xbox*"
+write $res_op
---
+++ hello-powershell/hellops1.ps1
@@ -0,0 +1,2 @@
+$servers=@('server1', 'server2')
+echo "The full domain name of the server is $($servers[1]).lt.com"
